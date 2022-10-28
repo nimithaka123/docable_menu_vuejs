a2820de9138b286fa76a2e3042286ffde2bdda51
nimithaka123 <nimithak@alokin.in>
2022-10-28 13:16:15 +0530

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="menu">
+        <MenuItem
+            v-for="(item,index) in menuTree"
+            :key="index"
+            :label="item.label"
+            :depth="0"
+            :data="item.children"
+        />
+    </div>
+</template>
+
+<script>
+import MenuItem from './MenuItem'
+
+export default{
+    name: "docmenu",
+    data: () => ({
+        menuTree: [
+            {   
+                label: "File",
+                children:[
+                    {
+                        label: "Open Folder",
+                    },
+                    {
+                        label: "Open Window",
+                    },
+                    {
+                        label: "Open Recent",
+                    },
+                ]
+            },
+            {
+                label: "Edit",
+                children:[
+                    {
+                        label: "Undo",
+                    },
+                    {
+                        label: "Redo",
+                    },
+                ]
+
+            },
+            {
+                label: "settings",
+            }
+        ]
+
+    }),
+    components:{
+        MenuItem
+    }
+
+ }
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.menu{
+    position: fixed;
+    height: 300px;
+    width: 150px;
+    background-color:#F7F9F9  ;
+    border-right: 1px solid ;
+    border-bottom: 1px solid ;
+    border-radius: 8px;
+    padding: 5px;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="menu-item">
+        <div class="label"   @click="toggleMenu()">
+            <span>{{label}}</span>
+        </div>
+        <div class="items-container" v-show="showChildren">
+            <menu-item
+                v-for="(item,index) in data"
+                :key="index"
+                :label="item.label"
+                :depth="depth + 1"
+                :data="item.children"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "docmenu",
+        data: () => ({
+            showChildren: false
+        }),
+        props: {
+            label: {
+                type: String,
+                required: true
+            },
+            depth: {
+                type: Number,
+                required: true
+            },
+            data: {
+                type: Array
+            }
+        },
+        methods: {
+            toggleMenu(){
+                this.showChildren = !this.showChildren
+            }
+        }
+
+
+    }
+</script>
+
+<style>
+.menu-item{
+    position: relative;
+    width: 100%;
+}
+.label{
+    width: 100%;
+    box-sizing: border-box;
+    color:black ;
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: bold;
+    font-size: 17px;
+}
+.label:hover{
+    background-color:#D5D8DC ;
+}
+.items-container{
+    margin:10px
+}
+</style>
